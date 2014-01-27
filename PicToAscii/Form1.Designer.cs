7f728c21e1a22a03ecde9ffce525cf27ff64ac84
Tom Mitchell <tom@tom.net.nz>
2014-01-27 14:49:23 -0800

    Initial commit

---
+++ PicToAscii/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PicToAscii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.fileImage = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(12, 33);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(799, 20);
+            this.txtFile.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(817, 31);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(12, 84);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(56, 20);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.Text = "80";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(764, 71);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(128, 33);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Image File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Maximum Width";
+            // 
+            // txtImage
+            // 
+            this.txtImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtImage.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImage.Location = new System.Drawing.Point(12, 120);
+            this.txtImage.Multiline = true;
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(880, 675);
+            this.txtImage.TabIndex = 8;
+            this.txtImage.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 807);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.MinimumSize = new System.Drawing.Size(340, 400);
+            this.Name = "Form1";
+            this.Text = "Image -> Ascii";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtImage;
+        private System.Windows.Forms.OpenFileDialog fileImage;
+    }
+}
+
---
+++ PicToAscii/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicToAscii
+{
+    public partial class Form1 : Form
+    {
+        // Constants for determining which letters to use
+        private const string _FONT_FACE = "Courier New";
+        private const int _FONT_SIZE = 12;
+        private int[] _AVAILABLE_CHAR_IDS = new int[] {
+            32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+            64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
+            80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+            96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+            110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+            123, 124, 125, 126, 176, 177, 178, 219, 254
+        };
+
+        private const double _LETTER_ASPECT_RATIO = 0.5;
+
+        // Cache the letter "density" values
+        private bool _brightnessesComputed = false;
+        private Dictionary<double, int> _brightnesses;
+        private double _minBrightness = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            // Just show the open file dialog, and place the result
+            // in the filename textbox if valid
+            var result = fileImage.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFile.Text = fileImage.FileName;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            // Parse the width/height
+            int maxWidth;
+            if (!int.TryParse(txtWidth.Text, out maxWidth))
+            {
+                MessageBox.Show("Please enter a valid number for maximum width");
+                return;
+            }
+
+            // Attempt to open the file
+            Image image = null;
+            try
+            {
+                image = Image.FromFile(txtFile.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please specify a valid filename for the input image");
+                return;
+            }
+
+            // Compute densities if needed
+            ComputeLetterDensities();
+
+            Bitmap bitmap = null;
+
+            // Do we need to resize?
+            int newWidth = Math.Min(image.Width, maxWidth);
+            double aspectRatio = (double)image.Width / (double)image.Height;
+            aspectRatio /= _LETTER_ASPECT_RATIO;
+
+            bitmap = new Bitmap(newWidth, (int)((double)newWidth / aspectRatio));
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawImage(image, 0, 0, bitmap.Width, bitmap.Height);
+            }
+
+            var brightnessKeys = _brightnesses.Keys.ToList();
+            brightnessKeys.Sort();
+            brightnessKeys.Reverse();
+
+            // Produce the string
+            // NOTE: This is currently SUPER inefficient - can probably be optimized
+            var output = new StringBuilder();
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    double brightness = (double)bitmap.GetPixel(x, y).GetBrightness();
+                    bool foundChar = false;
+                    foreach (var brightnessKey in brightnessKeys)
+                    {
+                        // Some finnicky math to convert brightness based
+                        // on the "most dense" character's actual brightness
+                        var brightnessKeyActual = (brightnessKey - _minBrightness) / (1 - _minBrightness);
+                        if (brightness > brightnessKeyActual)
+                        {
+                            output.Append((char)_brightnesses[brightnessKey]);
+                            foundChar = true;
+                            break;
+                        }
+                    }
+                    if (!foundChar)
+                    {
+                        // Just take the darkest one we know about
+                        output.Append((char)_brightnesses[brightnessKeys[brightnessKeys.Count - 1]]);
+                    }
+                }
+                output.Append(Environment.NewLine);
+            }
+
+            txtImage.Text = output.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ComputeLetterDensities()
+        {
+            if (_brightnessesComputed)
+            {
+                return;
+            }
+
+            _brightnesses = new Dictionary<double, int>();
+
+            // Build the font we want to use as a reference
+            var font = new Font(_FONT_FACE, _FONT_SIZE);
+            Bitmap bitmap = new Bitmap(1, 1);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            if (graphics.MeasureString("iii", font).Width != graphics.MeasureString("WWW", font).Width)
+                throw new Exception("Must use a fixed pitch font.");
+            
+            // Work out how big each letter is - assuming a fixed pitch font
+            StringFormat sf = new StringFormat(StringFormat.GenericTypographic);
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            SizeF charSize = graphics.MeasureString("█", font, new PointF(0, 0), sf);
+
+            int letterHeight = (int)charSize.Height + 4;
+            int letterWidth = (int)charSize.Width + 2;
+
+            // Compute densities for each letter
+            bitmap = new Bitmap(letterWidth, letterHeight, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            graphics = Graphics.FromImage(bitmap);
+
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            _minBrightness = 1;
+
+            foreach (int id in _AVAILABLE_CHAR_IDS)
+            {
+                // Clear the bitmap
+                graphics.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+
+                // Draw the letter
+                graphics.DrawString(((char)id).ToString(), font, new SolidBrush(Color.Black), 0, 0);
+
+                // Determine average pixel brightness as "density"
+                double totalBrightness = 0;
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        totalBrightness += (double)bitmap.GetPixel(x, y).GetBrightness();
+                    }
+                }
+                double brightness = totalBrightness / (double)(bitmap.Width * bitmap.Height);
+                if (!_brightnesses.ContainsKey(brightness))
+                {
+                    _brightnesses.Add(brightness, id);
+                    if (brightness < _minBrightness)
+                    {
+                        _minBrightness = brightness;
+                    }
+                }
+            }
+        }
+    }
+}
